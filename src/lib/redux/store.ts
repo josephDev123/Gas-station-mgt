86c34c0f9f8f73b3cc5eaa1a36bfcd177d9e3d55
josephDev123 <josephuzuegbu55@gmail.com>
2025-07-02 12:05:32 +0100

    axio instances

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,10 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./rootReducer";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { storage } from "./storage";
 
 export const store = configureStore({
   reducer: rootReducer,
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-// export let persistor = persistStore(store);
+export let persistor = persistStore(store);
